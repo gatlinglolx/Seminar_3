52e40addc2e9dd0003a2d0bc4c780c1c812f3f6e
vladislav shakhov <shahov199@gmail.com>
2022-12-23 18:29:11 +0300

    zadacha_23

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,13 @@
+﻿int Qwe(string name)
+{
+    Console.WriteLine(name);
+    int N = Convert.ToInt32(Console.ReadLine());
+    return N;
+}
+int N = Qwe("Enter N");
+string answer = "";
+for (int i = 1; i <= N; i++)
+{
+    answer = answer + Math.Pow(i, 3).ToString() + ", ";
+}
+Console.WriteLine(answer);
