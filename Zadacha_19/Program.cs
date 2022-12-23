ad224806183dc889244ddba11eb8bcbd470ae699
vladislav shakhov <shahov199@gmail.com>
2022-12-23 18:14:20 +0300

    zadacha_19

---
+++ Zadacha_19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+int firstNumber = number / 10000;
+int secondNumber = number / 1000;
+int secondNumberTwo = secondNumber % 10;
+int fourthNumber = number / 10;
+int fourthNumberFour = fourthNumber % 10;
+int fifthNumber = number % 10;
+if (firstNumber == fifthNumber && secondNumberTwo == fourthNumberFour)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
